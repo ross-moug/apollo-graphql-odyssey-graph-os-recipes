283f9a2685782fc5af3c41a851d7a340b24ba4ef
Ross Moug <ross.moug@n-able.com>
2024-01-10 09:22:05 +0000

    Add Recipe.cookware field

--- src/resolvers/Recipe.js
+++ src/resolvers/Recipe.js
@@ -7,5 +7,14 @@ module.exports = {
       const limit = 3;
       return dataSources.recipesAPI.getRelatedRecipes(recipe.id, limit);
     },
-  },
+    cookware(recipe, _, { dataSources }) {
+      const cookwareNamesList = dataSources.recipesAPI.getRecipeCookware(recipe.id);
+
+      if (!cookwareNamesList) {
+        return;
+      }
+
+      return cookwareNamesList.map((cookwareName) => ({ name: cookwareName }));
+    }
+  }
 };
